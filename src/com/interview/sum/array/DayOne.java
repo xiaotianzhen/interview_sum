2ba2eb7ef2e7682423fda585d8880311a28cf41b
dengchuandong <dengchuandong@51yund.com>
2023-02-21 21:53:30 +0800

    first commit

---
+++ src/com/interview/sum/array/DayOne.java
@@ -0,0 +1,14 @@
+package com.interview.sum.array;
+
+
+public class DayOne {
+
+    /***
+     * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+     * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hello_world");
+    }
+}
