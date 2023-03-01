64f2236737ff10a8a679a8b4e7754be88e1382bb
dengchuandong <dengchuandong@51yund.com>
2023-03-01 22:11:34 +0800

    first day

--- src/com/interview/sum/array/DayOne.java
+++ src/com/interview/sum/array/DayOne.java
@@ -9,6 +9,34 @@ public class DayOne {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("hello_world");
+        int[][] arr = new int[][]{{1, 3, 5, 7, 9}, {2, 4, 7, 9, 10}, {3, 5, 9, 10, 12}, {4, 6, 10, 11, 15}};
+        int n = 10;
+        //解题思路：从右上角，或者左上角开始找
+        //1, 3, 5, 7, 9
+        //2, 4, 7, 9, 10
+        //3, 5, 9, 10, 12
+        //4, 6, 10, 11, 15
+        int row = arr.length;
+        int column = arr[0].length;
+        if (n > arr[row - 1][column - 1] || n < arr[0][0]) {
+            System.out.println("不存在该整数");
+            return;
+        }
+        int r = 0;
+        int c = column - 1;
+        boolean isExist = false;
+        while (c > 0 && r < row) {
+            if (n > arr[r][c]) {
+                r++;
+            } else if (n < arr[r][c]) {
+                c--;
+            } else {
+                isExist = true;
+                System.out.println("存在该整数" + arr[r][c]);
+                System.out.println("存在该整数r "+r+" c "+c);
+                break;
+            }
+        }
+        System.out.println(isExist);
     }
 }
